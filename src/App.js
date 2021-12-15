5513b3d41e9b85bcba114ec35a0ef5482740cdcd
andreaguinder <andreabelen.guinder@gmail.com>
2021-12-15 18:52:51 -0300

    aprendiendo import y export

--- src/App.js
+++ src/App.js
@@ -1,26 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-
-// prueba de comentario en appjs
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//
+function App (){
+    return "Hola Mundo JSX"
 }
 
-export default App;
+export default App
