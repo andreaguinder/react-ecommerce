e251ab111986fc3bf1f26597b2b3552032b9b19d
andreaguinder <andreabelen.guinder@gmail.com>
2021-12-16 11:20:56 -0300

    font

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,9 +13,9 @@ function NavBar (){
             </div>
                 <nav>
                     <ul>
-                        <li><a href="#"><i className="fas fa-user"></i></a></li>
-                        <li><a href="#"><i className="fas fa-heart"></i></a></li>
-                        <li><a href="#"><i className="fas fa-shopping-cart"></i></a></li>
+                        <li><a href="#"><FontAwesomeIcon icon={faCoffee}/></a></li>
+                        <li><a href="#">corazon</a></li>
+                        <li><a href="#">shpopping cart</a></li>
                     </ul>
                 </nav>
         </header>
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@
 
 //
 import ReactDOM from "react-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 import App from "./App"
 
 import "./components/estilos/estilos.scss"
+
 //
 ReactDOM.render(<App/>,document.getElementById("root"));
 
