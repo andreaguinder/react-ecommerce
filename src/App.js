db8cbd740adb22eb87f6ab94cd8e7a28cdffb31e
andreaguinder <andreabelen.guinder@gmail.com>
2021-12-17 17:15:14 -0300

    desafio 5

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 //
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.scss';
+// import './App.scss';
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import Contador from "./containers/Contador";
