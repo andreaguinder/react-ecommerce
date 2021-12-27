844ca8887f4ba4109f93eeb5289a3f90853e5c79
andreaguinder <andreabelen.guinder@gmail.com>
2021-12-27 16:42:50 -0300

    mejorando el contador

--- src/containers/Contador.js
+++ src/containers/Contador.js
@@ -5,20 +5,16 @@ const Contador = ( {stock, initial, onAdd}) => {
     const [ Contador, setContador ] = useState(1);
 
     const sumar = () => {
-        if (Contador < stock){
         setContador(Contador + 1)
-        }
     }
     const restar = () => {
-        if (Contador > initial) {
             setContador(Contador - 1)
-        }
     }
 
     return (
     <div className="divCentrado">
-        <button onClick={() => restar()} disabled={Contador == initial}>-</button>
-        <p>{Contador}</p>
+        <button onClick={() => restar()} disabled={Contador === initial}>-</button>
+        <p>Cantidad Actual: {Contador}</p>
         <button onClick={() => sumar()} disabled={Contador >= stock}>+</button>
         <button> Agregar al Carrito </button>
     </div>
