8632dc1a171f83ce61035ffe26fe706548072aa4
andreaguinder <andreabelen.guinder@gmail.com>
2022-01-17 20:41:47 -0300

    trabajando el carrito

--- src/components/Carrito.js
+++ src/components/Carrito.js
@@ -1,25 +1,30 @@
-import React from 'react'
-import { useContext } from 'react'
-import { Link } from 'react-router-dom';
-import { CartContext } from '../context/CartContext'
-import CartItem from './CartItem';
+import React from "react";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { CartContext } from "../context/CartContext";
+import CartItem from "./CartItem";
 
 const CartContainer = () => {
+  const { cartArray, deleteItem } = useContext(CartContext);
 
-    const { cartArray, deleteItem } = useContext(CartContext);
+  return (
+    <div>
+      {cartArray.length === 0 && (
+        <div>
+          <p>No hay productos aún</p>
+          <Link to="/">Ir al inicio</Link>
+        </div>
+      )}
+      {cartArray.length > 0 &&
+        cartArray.map((product) => (
+          <CartItem
+            key={product.item.id}
+            product={product}
+            deleteItem={deleteItem}
+          />
+        ))}
+    </div>
+  );
+};
 
-    return (
-      <div>
-        {cartArray.length === 0 &&
-          <div>
-            <p>No hay productos aún</p>
-            <Link to='/'>Ir al inicio</Link>
-          </div>
-        }
-        {(cartArray.length > 0) && cartArray.map(product => <CartItem key={product.item.id} product={product} deleteItem={deleteItem} />)}
-      </div>
-    )
-  }
-  
-  export default CartContainer;
-
+export default CartContainer;
