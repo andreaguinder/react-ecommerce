616d133f8eebdede973c38200026482e0bc62cf7
andreaguinder <andreabelen.guinder@gmail.com>
2022-01-17 15:33:24 -0300

    mejoras de render

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -4,10 +4,10 @@ import { useParams } from "react-router-dom"
 
 const productos = [
     {id: 1, nombre: "Cuadro Dalí", precio: 5000, img: "/img/pruebaimg.jpg", categoria: "cuadros"},
-    {id: 2, nombre: "Anotadores s/e", precio: 500, img: "/img/anotadores.jpg", categoria: "papeleria"},
-    {id: 3, nombre: "Cuadro Dalí 2", precio: 5000, img: "/img/tmp1091343711360516099.jpg", categoria: "cuadros"},
+    {id: 2, nombre: "Anotadores S/E", precio: 500, img: "/img/anotadores.jpg", categoria: "papeleria"},
+    {id: 3, nombre: "Galatea de las Esferas", precio: 5000, img: "/img/tmp1091343711360516099.jpg", categoria: "cuadros"},
     {id: 4, nombre: "Velas Strawberry", precio: 200, img: "/img/Velas mockup con trama.png", categoria: "velasYaroma"},
-    {id: 5, nombre: "Cuadro Dalí", precio: 800, img: "/img/fondo 2.jpg", categoria: "cuadros"}
+    {id: 5, nombre: "Cuadro Naturaleza", precio: 800, img: "/img/fondo 2.jpg", categoria: "cuadros"}
 ]
 const ItemListContainer = ({ titulo }) => {
 
