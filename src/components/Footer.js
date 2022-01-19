a8dc45a4639d3ec7438466353a77c8af78e7cedf
andreaguinder <andreabelen.guinder@gmail.com>
2022-01-19 17:09:57 -0300

    todos los cambios en master

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const FooterConst = () => {
     return (
     <>
     <Container id='footer' fluid style={{width: "100", height: "auto", marginBottom: "0", alignSelf: "flex-end"}}>
-                <Col style={{display: "flex", flexFlow: "column", flexWrap: "wrap", justifyContent: "space-evenly",}}>
+                <Col style={{display: "flex", flexFlow: "column", flexWrap: "wrap", justifyContent: "space-evenly"}}>
                     <Link to='#' className="linksEstilo">Legales</Link>
                     <Link to='#' className="linksEstilo">Nosotros</Link>
                     <Link to='#' className="linksEstilo">¿Cómo Comprar?</Link>
